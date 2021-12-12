f3eb7598065ee9b42ca4af6a77f227f983975ad2
musicm122 <terrance@hackerferret.com>
2021-12-12 15:05:25 -0500

    Fix missing fsproject ref

--- lvl1.cs
+++ lvl1.cs
@@ -10,10 +10,6 @@ public class lvl1 : lvl1Fs
     // private string b = "text";
 
     // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
-    {
-        
-    }
 
 //  // Called every frame. 'delta' is the elapsed time since the previous frame.
 //  public override void _Process(float delta)
